30f3c77fcde4ba384e14d3f80a3dcbdb5d2f0678
Ramadeep Yadav <rdeep1208y@gmail.com>
2026-07-12 21:50:56 +0530

    Create CheckInteger.java

---
+++ Lab_08/CheckInteger.java
@@ -0,0 +1,28 @@
+class CheckInteger
+{
+    public static void main(String[] args) {
+        int valid = 0;
+        int invalid = 0;
+
+
+        for(int i=0; i<args.length; i++){
+            boolean isNum = true;
+
+
+            for(int j=0; j<args[i].length(); j++){
+                char ch = args[i].charAt(j);
+                if(!Character.isDigit(ch)){
+                    isNum = false;
+                    break;
+                }
+            }
+            if(isNum){
+                valid ++;           }
+            else{
+                invalid++;
+            }
+        }
+       
+        System.out.println("valid integers: " +valid);
+        System.out.println("Invalid integers: " +invalid);
+    }
